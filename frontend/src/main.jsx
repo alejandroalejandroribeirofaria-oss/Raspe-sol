016a2cbdeb14a34b2abd65274e04d9b58f8a679b
alejandroalejandroribeirofaria-oss <alejandroalejandroribeirofaria@gmail.com>
2026-07-18 12:28:02 -0300

    Update main.jsx

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,13 +1,15 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import App from './App.jsx';
-import { I18nProvider } from './i18n/I18nContext.jsx';
-import './styles/index.css';
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import App from './App.jsx'
+import { I18nProvider } from './i18n/I18nProvider.jsx'
+import { WalletProvider } from './wallet/WalletProvider.jsx' // 1. IMPORTA
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <I18nProvider>
-      <App />
+      <WalletProvider>  {/* 2. ENVOLVE O APP */}
+        <App />
+      </WalletProvider>
     </I18nProvider>
-  </React.StrictMode>
-);
+  </React.StrictMode>,
+)
