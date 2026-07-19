1bb1214bdb0992a8134261ff275ad1510d1a4125
alejandroalejandroribeirofaria-oss <alejandroalejandroribeirofaria@gmail.com>
2026-07-18 23:44:42 -0300

    Import wallet adapter styles and reorder providers
    Added styles for wallet modal and adjusted provider order.

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,7 +1,9 @@
-import './styles/index.css' 
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
+import './styles/index.css' 
+import '@solana/wallet-adapter-react-ui/styles.css' // <- PRECISA DISSO PRA MODAL DA WALLET FUNCIONAR
+
 import { I18nProvider } from './i18n/I18nProvider.jsx'
 import { WalletProvider } from './wallet/WalletProvider.jsx'
 import { ChatProvider } from './chat/ChatProvider.jsx'
@@ -9,8 +11,8 @@ import { ChatProvider } from './chat/ChatProvider.jsx'
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <I18nProvider>
-      <WalletProvider> {/* 1º: Wallet */}
-        <ChatProvider> {/* 2º: Chat usa Wallet */}
+      <WalletProvider> {/* 1º: Wallet tem que vir primeiro */}
+        <ChatProvider> {/* 2º: Chat usa o Wallet */}
           <App />
         </ChatProvider>
       </WalletProvider>
