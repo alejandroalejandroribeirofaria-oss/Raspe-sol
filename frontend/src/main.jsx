ed45ba12c089b2aefc03a00ac2e7dde8c1a3d1f8
alejandroalejandroribeirofaria-oss <alejandroalejandroribeirofaria@gmail.com>
2026-07-18 21:04:17 -0300

    Integrate ChatProvider with WalletProvider in main.jsx
    Added ChatProvider to wrap the App component.

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,14 +1,18 @@
+import './styles/index.css' // <-- COLOCA ESSA PRIMEIRA LINHA
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import { I18nProvider } from './i18n/I18nProvider.jsx'
-import { WalletProvider } from './wallet/WalletProvider.jsx' // 1. IMPORTA
+import { WalletProvider } from './wallet/WalletProvider.jsx'
+import { ChatProvider } from './chat/ChatProvider.jsx'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <I18nProvider>
-      <WalletProvider>  {/* 2. ENVOLVE O APP */}
-        <App />
+      <WalletProvider>
+        <ChatProvider>
+          <App />
+        </ChatProvider>
       </WalletProvider>
     </I18nProvider>
   </React.StrictMode>,
