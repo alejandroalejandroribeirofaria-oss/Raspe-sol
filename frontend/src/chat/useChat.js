a3c432df6263a2609b4b2d87143553de96163e49
alejandroalejandroribeirofaria-oss <alejandroalejandroribeirofaria@gmail.com>
2026-07-19 00:16:53 -0300

    Delete frontend/src/chat/useChat.js

--- frontend/src/chat/useChat.js
+++
@@ -1,9 +0,0 @@
-import { useContext } from 'react';
-import { ChatContext } from './ChatContext.jsx';
-
-export function useChat() {
-  const ctx = useContext(ChatContext);
-  if (!ctx) throw new Error('useChat must be used within <ChatProvider>.');
-  return ctx;
-}
-
