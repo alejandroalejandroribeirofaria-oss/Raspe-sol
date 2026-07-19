41a16192b9312c0eca042c3f8f6ff7bf0e909ce3
alejandroalejandroribeirofaria-oss <alejandroalejandroribeirofaria@gmail.com>
2026-07-19 15:26:34 -0300

    Include ChatToggleButton in main.jsx
    Added ChatToggleButton component to the main render.

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,13 +1,13 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-
 import App from './App.jsx'
 import './styles/index.css'
-import '@solana/wallet-adapter-react-ui/styles.css' // estilos da wallet
+import '@solana/wallet-adapter-react-ui/styles.css'
 
 import { I18nProvider } from './i18n/I18nProvider.jsx'
 import { WalletProvider } from './wallet/WalletProvider.jsx'
 import { ChatProvider } from './chat/ChatProvider.jsx'
+import ChatToggleButton from './chat/ChatToggleButton.jsx' // <- FALTAVA ISSO
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
@@ -15,6 +15,7 @@ ReactDOM.createRoot(document.getElementById('root')).render(
       <WalletProvider>
         <ChatProvider>
           <App />
+          <ChatToggleButton /> {/* <- FALTAVA ISSO TAMBÉM */}
         </ChatProvider>
       </WalletProvider>
     </I18nProvider>
